6a86e5d111a12c2431dfdbdbfc4960f8f1068836
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-07-19 19:29:46 -0300

    feat: configured application eslint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,14 +4,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head> 
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
 
-      <style id="stitches" dangerouslySetInnerHTML={{__html: getCssText()}}/>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
       </Head>
-      <body >
+      <body>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <h1>Pagina home</h1>
-  )
+  return <h1>Pagina home</h1>
 }
